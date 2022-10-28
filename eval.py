ee24c434f609494bf541e9776fd7a9bb4d016faa
Richard Rikk <rikk.richard@gmail.com>
2022-10-28 20:18:22 +0200

    eval added

---
+++ eval.py
@@ -0,0 +1,36 @@
+import ray
+import gym
+from env import CartPoleRightLeft
+
+from ray.rllib.algorithms import dqn
+
+save_dir = "videos/"
+env = CartPoleRightLeft(env_config={})
+env = gym.wrappers.RecordVideo(env=env, video_folder=save_dir)
+
+ray.init()
+
+config = dqn.DQNConfig()
+config.training(
+    num_atoms=2,
+    v_min=0.,
+    v_max=200.,
+    noisy=True,
+    dueling=True,
+    double_q=True,
+    hiddens=[128,128],
+    n_step=5
+).resources(num_gpus=1).framework(framework="torch")
+
+model = dqn.DQN(env=CartPoleRightLeft,config=config)
+model.restore("save/checkpoint_001940")
+
+done    = False
+obs     = env.reset()
+policy  = model.get_policy()
+
+while not done:
+  action, _, _ = policy.compute_single_action(obs=obs)
+  obs, _, done, _ = env.step(action=action)
+
+
